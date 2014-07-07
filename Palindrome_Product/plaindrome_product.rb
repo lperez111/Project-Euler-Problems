1b99b2d80f8cfd674853803a7080374b797601ca
Luis Perez <lperez111@gmail.com>
2014-07-06 20:46:50 -0500

    Solved Palindrome probelm

---
+++ Palindrome_Product/plaindrome_product.rb
@@ -0,0 +1,12 @@
+def palindrome
+	array = []
+	999.downto(100) do |x|
+		999.downto(100) do |y|
+			product = x*y
+			array << product if product.to_s == product.to_s.reverse
+		end
+	end
+	array.max
+end
+
+palindrome
